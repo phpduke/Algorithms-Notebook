9265d9a36d3b9b2a0dde304196a2d7fd0c752331
Manas Kumar Verma <fazura_help@ymail.com>
2018-12-15 23:52:19 +0530

    Update random_distributions.cpp

--- random_distributions.cpp
+++ random_distributions.cpp
@@ -1,4 +1,5 @@
 //C++ 11 onwards
+//Historgram Playground commented at bottom
 #include <random>
 #include <iostream>
  
@@ -27,3 +28,22 @@ int main()
         std::cout << dis(gen) << ' ';
     std::cout << '\n';
 }
+
+/*
+int main()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    // perform 4 trials, each succeeds 1 in 2 times
+    std::binomial_distribution<> d(4, 0.5);
+ 
+    std::map<int, int> hist;
+    for (int n = 0; n < 10000; ++n) {
+        ++hist[d(gen)];
+    }
+    for (auto p : hist) {
+        std::cout << p.first << ' '
+                  << std::string(p.second/100, '*') << '\n';
+    }
+}
+*/
