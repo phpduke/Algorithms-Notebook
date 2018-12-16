2ccf9466340fcf231134e1f36ebbe8099d80f497
Manas Kumar Verma <fazura_help@ymail.com>
2018-12-16 18:33:30 +0530

    Create bit tricks.cpp

---
+++ bit tricks.cpp
@@ -0,0 +1,7 @@
+unsigned int v; // current permutation of bits 
+unsigned int w; // next permutation of bits
+
+unsigned int t = v | (v - 1); // t gets v's least significant 0 bits set to 1
+// Next set to 1 the most significant bit to change, 
+// set to 0 the least significant ones, and add the necessary 1 bits.
+w = (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1));
