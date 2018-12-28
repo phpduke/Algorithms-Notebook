b810526e5f9d9c41adfda6f5042333573fdb78a7
Manas Kumar Verma <fazura_help@ymail.com>
2018-12-28 15:32:13 +0530

    Update phi.cpp

--- phi.cpp
+++ phi.cpp
@@ -1,16 +1,17 @@
-\emph{Euler's totient} or \emph{Euler's phi} function is defined as $\phi(n):=\#$ of positive integers $\leq n$ that are coprime with $n$. The \emph{cototient} is $n-\phi(n)$.
- * $\phi(1)=1$, $p$ prime $\Rightarrow \phi(p^k)=(p-1)p^{k-1}$, $m,n$ coprime $\Rightarrow \phi(mn)=\phi(m)\phi(n)$.
- * If $n=p_1^{k_1}p_2^{k_2} ... p_r^{k_r}$ then $\phi(n) = (p_1-1)p_1^{k_1-1}...(p_r-1)p_r^{k_r-1}$.
- * $\phi(n)=n \cdot \prod_{p|n}(1-1/p)$.
- * $\sum_{d|n} \phi(d) = n$, $\sum_{1\leq k \leq n, \gcd(k,n)=1} k = n \phi(n)/2, n>1$
- * \textbf{Euler's thm}: $a,n$ coprime $\Rightarrow a^{\phi(n)} \equiv 1 \pmod{n}$.
- * \textbf{Fermat's little thm}: $p$ prime $\Rightarrow a^{p-1} \equiv 1 \pmod{p}$ $\forall a$.
- * Status: Tested
- */
-const int LIM = 5000000;
-int phi[LIM];
+const int LIM = 5000000; int phi[LIM];
 void calculatePhi() {
 	rep(i,0,LIM) phi[i] = i&1 ? i : i/2;
 	for(int i = 3; i < LIM; i += 2) if(phi[i] == i)
 		for(int j = i; j < LIM; j += i) phi[j] -= phi[j] / i;
+}
+//Mobius Inversion : Let required be {sum_of_g=1-to-Gmax}h(g)*cnt[g]
+//Then h(g)={sum_of_d:d|g}f(d) //f(n)={sum_of_d:d|n}f(d)*mobius(n/d)
+FOR(i,1,n) for (int j=1;j<=n;j+=i) f[j] += h[i] * µ[j/i];
+void preprocess() { // Compute mobius fucntion mu[]
+    FOR(i,1,111100) mu[i]=primechk[i]=1;primechk[1]=0;
+    FOR(i,2,111100) {
+        if(primechk[i]==0) continue;
+        mu[i]=-mu[i];
+        for (int j=2;i*j<=111100;j++) primechk[i*j]=0,mu[i*j]=(j%i==0?0:mu[i*j]=-mu[i*j];
+    }
 }
