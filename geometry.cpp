cacea44d1bc7922f0a3dd7ac65137f89fffce108
Manas Kumar Verma <fazura_help@ymail.com>
2018-12-28 15:51:00 +0530

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -11,27 +11,58 @@ struct PT {
   PT operator - (const PT &p)  const { return PT(x-p.x, y-p.y); }
   PT operator * (double c)     const { return PT(x*c,   y*c  ); }
   PT operator / (double c)     const { return PT(x/c,   y/c  ); }
+  bool operator<(const PT &rhs) const { return make_pair(y,x) < make_pair(rhs.y,rhs.x); }
+  bool operator==(const PT &rhs) const { return make_pair(y,x) == make_pair(rhs.y,rhs.x); }
 };
 
 double dot(PT p, PT q)     { return p.x*q.x+p.y*q.y; }
 double dist2(PT p, PT q)   { return dot(p-q,p-q); }
 double cross(PT p, PT q)   { return p.x*q.y-p.y*q.x; }
-ostream &operator<<(ostream &os, const PT &p) {
-  return os << "(" << p.x << "," << p.y << ")"; 
-}
+ostream &operator<<(ostream &os,const PT &p) {return os<<"("<<p.x<<","<<p.y<<")";}
 
 // rotate a point CCW or CW around the origin
 PT RotateCCW90(PT p)   { return PT(-p.y,p.x); }
 PT RotateCW90(PT p)    { return PT(p.y,-p.x); }
-PT RotateCCW(PT p, double t) { 
-  return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t)); 
-}
+PT RotateCCW(PT p, double t) {return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t));}
+//<hull>
+#define REMOVE_REDUNDANT
+double area2(PT a, PT b, PT c) { return cross(a,b) + cross(b,c) + cross(c,a); }
+#ifdef REMOVE_REDUNDANT
+bool between(const PT &a, const PT &b, const PT &c) {
+  return (fabs(area2(a,b,c)) < EPS && (a.x-b.x)*(c.x-b.x) <= 0 && (a.y-b.y)*(c.y-b.y) <= 0);}
+#endif
+void ConvexHull(vector<PT> &pts) {
+  sort(pts.begin(), pts.end());
+  pts.erase(unique(pts.begin(), pts.end()), pts.end());
+  vector<PT> up, dn;
+  for (int i = 0; i < pts.size(); i++) {
+    while (up.size() > 1 && area2(up[up.size()-2], up.back(), pts[i]) >= 0) up.pop_back();
+    while (dn.size() > 1 && area2(dn[dn.size()-2], dn.back(), pts[i]) <= 0) dn.pop_back();
+    up.push_back(pts[i]);
+    dn.push_back(pts[i]);
+  }
+  pts = dn;
+  for (int i = (int) up.size() - 2; i >= 1; i--) pts.push_back(up[i]);
+#ifdef REMOVE_REDUNDANT
+  if (pts.size() <= 2) return;
+  dn.clear();
+  dn.push_back(pts[0]);
+  dn.push_back(pts[1]);
+  for (int i = 2; i < pts.size(); i++) {
+    if (between(dn[dn.size()-2], dn[dn.size()-1], pts[i])) dn.pop_back();
+    dn.push_back(pts[i]);
+  }
+  if (dn.size() >= 3 && between(dn.back(), dn[0], dn[1])) {
+    dn[0] = dn.back();
+    dn.pop_back();
+  }
+  pts = dn;
+#endif
+} // </hull>
 
 // project point c onto line through a and b
 // assuming a != b
-PT ProjectPointLine(PT a, PT b, PT c) {
-  return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
-}
+PT ProjectPointLine(PT a, PT b, PT c) {return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);}
 
 // project point c onto line segment through a and b
 PT ProjectPointSegment(PT a, PT b, PT c) {
@@ -56,15 +87,12 @@ double DistancePointSegment(PT a, PT b, PT c) {
 
 // compute distance between point (x,y,z) and plane ax+by+cz=d
 double DistancePointPlane(double x, double y, double z,
-                          double a, double b, double c, double d)
-{
+                          double a, double b, double c, double d) {
   return fabs(a*x+b*y+c*z-d)/sqrt(a*a+b*b+c*c);
 }
 
 // determine if lines from a to b and c to d are parallel or collinear
-bool LinesParallel(PT a, PT b, PT c, PT d) { 
-  return fabs(cross(b-a, c-d)) < EPS; 
-}
+bool LinesParallel(PT a, PT b, PT c, PT d) { return fabs(cross(b-a, c-d)) < EPS; }
 
 bool LinesCollinear(PT a, PT b, PT c, PT d) { 
   return LinesParallel(a, b, c, d)
@@ -126,8 +154,7 @@ bool PointInPolygon(const vector<PT> &p, PT q) {
 // determine if point is on the boundary of a polygon
 bool PointOnPolygon(const vector<PT> &p, PT q) {
   for (int i = 0; i < p.size(); i++)
-    if (dist2(ProjectPointSegment(p[i], p[(i+1)%p.size()], q), q) < EPS)
-      return true;
+    if (dist2(ProjectPointSegment(p[i], p[(i+1)%p.size()], q), q) < EPS) return true;
     return false;
 }
 
@@ -184,9 +211,7 @@ double ComputeSignedArea(const vector<PT> &p) {
   return area / 2.0;
 }
 
-double ComputeArea(const vector<PT> &p) {
-  return fabs(ComputeSignedArea(p));
-}
+double ComputeArea(const vector<PT> &p) {return fabs(ComputeSignedArea(p));}
 
 PT ComputeCentroid(const vector<PT> &p) {
   PT c(0,0);
@@ -197,7 +222,6 @@ PT ComputeCentroid(const vector<PT> &p) {
   }
   return c / scale;
 }
-
 // tests whether or not a given polygon (in CW or CCW order) is simple
 bool IsSimple(const vector<PT> &p) {
   for (int i = 0; i < p.size(); i++) {
