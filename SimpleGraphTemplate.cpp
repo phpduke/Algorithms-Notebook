30d54025e61655c8f07d436ce6d17751f4b772f2
Manas Kumar Verma <fazura_help@ymail.com>
2024-08-25 15:00:41 +0530

    Update SimpleGraphTemplate.cpp
    simplified distance array

--- SimpleGraphTemplate.cpp
+++ SimpleGraphTemplate.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <unordered_map>
 
 using namespace std;
 
 template <typename T>
 class Graph {
 private:
-    unordered_map<T, vector<T>> adj_list;
+    vector<vector<T>> adj_list;
     bool directed;
 
 public:
-    Graph(bool is_directed = false) : directed(is_directed) {}
+    Graph(int n, bool is_directed = false) : adj_list(n), directed(is_directed) {}
 
     void add_edge(const T& u, const T& v) {
         adj_list[u].push_back(v);
@@ -21,11 +20,8 @@ public:
         }
     }
 
-    vector<pair<T, int>> bfs(const T& start) {
-        unordered_map<T, int> distance;
-        for (const pair<T, vector<T>>& pair : adj_list) {
-            distance[pair.first] = -1;
-        }
+    vector<int> bfs(const T& start, const vector<bool>& landmines) {
+        vector<int> distance(adj_list.size(), -1);
         distance[start] = 0;
 
         queue<T> q;
@@ -36,55 +32,42 @@ public:
             q.pop();
 
             for (const T& neighbor : adj_list[current_node]) {
-                if (distance[neighbor] == -1) {
+                if (distance[neighbor] == -1 && !landmines[neighbor]) {
                     distance[neighbor] = distance[current_node] + 1;
                     q.push(neighbor);
                 }
             }
         }
 
-        vector<pair<T, int>> distance_vec(distance.begin(), distance.end());
-        return distance_vec;
-    }
-
-    void print_graph() const {
-        for (const pair<T, vector<T>>& pair : adj_list) {
-            cout << pair.first << ": ";
-            for (const T& neighbor : pair.second) {
-                cout << neighbor << " ";
-            }
-            cout << "\n";
-        }
+        return distance;
     }
 };
 
-// Example usage
 int main() {
-    Graph<int> g(false);  // Undirected graph
+    int N, M;
+    cin >> N >> M;
 
-    g.add_edge(1, 2);
-    g.add_edge(1, 3);
-    g.add_edge(2, 4);
-    g.add_edge(3, 5);
-    g.add_edge(4, 5);
+    int source, target;
+    cin >> source >> target;
 
-    // Print the adjacency list (for debugging)
-    cout << "Adjacency List:\n";
-    g.print_graph();
-
-    // Perform BFS starting from node 1
-    vector<pair<int, int>> distances = g.bfs(1);
-
-    cout << "\nDistances from node 1:\n";
-    for (int i = 0; i < distances.size(); ++i) {
-        cout << "Node " << distances[i].first << ": Distance ";
-        if (distances[i].second == -1) {
-            cout << "Infinity";
-        } else {
-            cout << distances[i].second;
-        }
-        cout << "\n";
+    vector<bool> landmines(N);
+    for (int i = 0; i < N; ++i) {
+        int has_landmine;
+        cin >> has_landmine;
+        landmines[i] = (has_landmine == 1);
     }
+
+    Graph<int> g(N, false);  // Undirected graph
+
+    for (int i = 0; i < M; ++i) {
+        int u, v;
+        cin >> u >> v;
+        g.add_edge(u, v);
+    }
+
+    vector<int> distances = g.bfs(source, landmines);
+
+    cout << distances[target] << "\n";
 
     return 0;
 }
